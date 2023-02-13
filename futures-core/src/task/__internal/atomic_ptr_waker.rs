37a66c933b440bd3fa305571ab3f20fae2ab3d99
Boqun Feng <boqun.feng@gmail.com>
2023-02-12 21:49:02 -0800

    Change to two function implementation
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- futures-core/src/task/__internal/atomic_ptr_waker.rs
+++ futures-core/src/task/__internal/atomic_ptr_waker.rs
@@ -1,6 +1,6 @@
 use core::cell::UnsafeCell;
 use core::fmt;
-use core::ptr::addr_of;
+use core::ptr::{addr_of, null_mut};
 use core::task::{RawWaker, RawWakerVTable, Waker};
 
 use atomic::AtomicPtr;
@@ -17,32 +17,23 @@ pub struct AtomicWaker {
     vtable: AtomicPtr<RawWakerVTable>,
 }
 
-unsafe fn dummy_clone1(_: *const ()) -> RawWaker {
-    panic!("You shall not clone1")
+unsafe fn dummy_clone_register(_: *const ()) -> RawWaker {
+    panic!("You shall not clone, this is only for register() locks")
 }
 
-unsafe fn dummy_clone2(_: *const ()) -> RawWaker {
-    panic!("You shall not clone2")
-}
-
-unsafe fn dummy_clone3(_: *const ()) -> RawWaker {
-    panic!("You shall not clone3")
+unsafe fn dummy_clone_take(_: *const ()) -> RawWaker {
+    panic!("You shall not clone, this is only for take() locks")
 }
 
 unsafe fn dummy_fn(_: *const ()) {
     panic!("You shall not wake or drop");
 }
 
-static TAKING_TABLE: RawWakerVTable =
-    RawWakerVTable::new(dummy_clone1, dummy_fn, dummy_fn, dummy_fn);
-static TAKEN_TABLE: RawWakerVTable =
-    RawWakerVTable::new(dummy_clone2, dummy_fn, dummy_fn, dummy_fn);
+// Uses two different `clone()` to avoid different tables optimized into one.
 static REGISTERING_TABLE: RawWakerVTable =
-    RawWakerVTable::new(dummy_clone3, dummy_fn, dummy_fn, dummy_fn);
-
-//const REGISTERING: *const RawWakerVTable = addr_of!(REGISTERING_TABLE);
-//const TAKING: *const RawWakerVTable = addr_of!(TAKING_TABLE);
-//const TAKEN: *const RawWakerVTable = addr_of!(TAKEN_TABLE);
+    RawWakerVTable::new(dummy_clone_register, dummy_fn, dummy_fn, dummy_fn);
+static TAKING_TABLE: RawWakerVTable =
+    RawWakerVTable::new(dummy_clone_take, dummy_fn, dummy_fn, dummy_fn);
 
 const fn lock_val(r: &RawWakerVTable) -> *mut RawWakerVTable {
     r as *const _ as _
@@ -51,7 +42,6 @@ const fn lock_val(r: &RawWakerVTable) -> *mut RawWakerVTable {
 #[derive(PartialEq)]
 enum Key {
     REGISTERING,
-    TAKEN,
     TAKING,
 }
 
@@ -61,36 +51,44 @@ fn to_mut(k: Key) -> *mut RawWakerVTable {
     match k {
         REGISTERING => addr_of!(REGISTERING_TABLE) as *mut _,
         TAKING => addr_of!(TAKING_TABLE) as *mut _,
-        TAKEN => addr_of!(TAKEN_TABLE) as *mut _,
     }
 }
 
 impl AtomicWaker {
-    pub fn new() -> Self {
-        Self { data: UnsafeCell::new(core::ptr::null()), vtable: AtomicPtr::new(to_mut(TAKEN)) }
+    pub const fn new() -> Self {
+        Self { data: UnsafeCell::new(core::ptr::null()), vtable: AtomicPtr::new(null_mut()) }
     }
 
-    pub fn register(&self, waker: &Waker) {
-        // register locking rules:
-        // * lock with REGISTERING
+    //    struct Registration<'a, 'b>(&'a Self);
+
+    /// Begins to register a [`Waker`], [`Err`] returns indicate unable to acquire the lock,
+    /// `Err(true)` means a racing taker observed, note that `Err(false)` doesn't mean that there
+    /// is no taker before this registration, in other words `Err(false)` does NOT imply that some
+    /// one will do the wake-up later.
+    #[inline]
+    fn begin_register(&self) -> Result<(), bool> {
+        // `register()` locking:
+        //
+        // * lock with REGISTERING, lose to REGISTERING or TAKING.
         // * unlock with real vtable (only done by the winning register)
+
         let old = self.vtable.swap(to_mut(REGISTERING), AcqRel);
 
         if old == to_mut(REGISTERING) {
+            //debug_assert!(false);
             // Lose race with another register, do nothing.
             // Also no wake up guarantee if this happens.
+            //
+            // And we don't whether a taker races with us.
+
+            Err(false)
         } else if old == to_mut(TAKING) {
             // Lose race with a taker, do nothing about the `.vtable`, the winning taker will unlock
             // it eventually.
 
-            // Do a wake up here since it's unable to set the new waker and we don't want to miss
-            // wake-ups.
-            //
-            // Potential optimization: if taking a waker means `Poll::Ready` for example, timers,
-            // we may not need wake-ups.
-            waker.wake_by_ref(); // re-poll
+            Err(true)
         } else {
-            if old != to_mut(TAKEN) {
+            if old != null_mut() {
                 // old waker still there, need to drop it
 
                 let data = unsafe { *(self.data.get()) };
@@ -101,45 +99,73 @@ impl AtomicWaker {
                 }
             }
 
-            // Set the new waker.
-            let waker = waker.clone();
-            unsafe {
-                *(self.data.get()) = waker.as_raw().data();
-            }
+            Ok(())
+        }
+    }
 
-            let _ = self
-                .vtable
-                .compare_exchange(
-                    to_mut(REGISTERING),
-                    lock_val(waker.as_raw().vtable()),
-                    AcqRel,
-                    Acquire,
-                )
-                .map_err(|v| {
-                    // Could only happen because of a racing taker.
-                    debug_assert_eq!(v, to_mut(TAKING));
+    /// The final action for registration, returning `true` means we observe a racing taker, unlike
+    /// `begin_register()`, we may miss a racing taker because the ABA behavior of CAS: REGISTERING
+    /// -> TAKING -> REGISTERING, but this only happens if there is a racing `register()`, and we
+    /// don't guarantee no missing wake-up for that, so callers can simply skip a self wake-up if
+    /// this returns false
+    #[inline]
+    fn end_register(&self, waker: &Waker) -> bool {
+        // Set the new waker.
+        let waker = waker.clone();
 
-                    // Race with a taker in the middle of setting the new waker
-                    //
-                    // Potential optimization: if taking a waker means `Poll::Ready` for example, timers,
-                    // we may not need wake-ups.
-                    waker.wake_by_ref();
+        unsafe {
+            *(self.data.get()) = waker.as_raw().data();
+        }
 
-                    self.vtable.store(lock_val(waker.as_raw().vtable()), Release);
-                });
+        if let Err(v) = self.vtable.compare_exchange(
+            to_mut(REGISTERING),
+            lock_val(waker.as_raw().vtable()),
+            AcqRel,
+            Acquire,
+        ) {
+            // Could only happen because of a racing taker.
+            debug_assert_eq!(v, to_mut(TAKING));
 
+            self.vtable.swap(null_mut(), Release);
+            true
+        } else {
             core::mem::forget(waker); // the `self` now owns `waker`.
+            false
+        }
+    }
+
+    pub fn register(&self, waker: &Waker) {
+        match self.begin_register() {
+            Ok(_) => {
+                // Lock acquired, do the rest work.
+                if self.end_register(waker) {
+                    // Always do a self wakeup
+                    waker.wake_by_ref();
+                }
+            }
+            Err(true) => {
+                waker.wake_by_ref();
+            }
+            Err(_) => {
+                waker.wake_by_ref();
+            }
         }
     }
 
     pub fn take(&self) -> Option<Waker> {
+        // `take()` locking:
+        //
+        // * lock with TAKING, lose to REGISTERING or TAKING.
+        // * unlock with `null_mut()` (only done by the winning register)
+
         let old = self.vtable.swap(to_mut(TAKING), AcqRel);
 
         if old == to_mut(REGISTERING) || old == to_mut(TAKING) {
             // lose race, nothing to do.
             None
         } else {
-            let res = if old == to_mut(TAKEN) {
+            let res = if old == null_mut() {
+                // Already taken.
                 None
             } else {
                 let data = unsafe { *(self.data.get()) };
@@ -147,7 +173,8 @@ impl AtomicWaker {
                 Some(unsafe { Waker::from_raw(RawWaker::new(data, &*old)) })
             };
 
-            self.vtable.store(to_mut(TAKEN), Release);
+            // Unlocks no matter whether there is a [`Waker`] to take or not.
+            self.vtable.store(null_mut(), Release);
 
             res
         }
--- futures-core/src/task/__internal/mod.rs
+++ futures-core/src/task/__internal/mod.rs
@@ -1,6 +1,6 @@
+mod atomic_ptr_waker;
 #[cfg(not(futures_no_atomic_cas))]
 mod atomic_waker;
-mod atomic_ptr_waker;
 #[cfg(not(futures_no_atomic_cas))]
 //pub use self::atomic_waker::AtomicWaker;
 pub use self::atomic_ptr_waker::AtomicWaker;
